06b12b105ead08ed24b10e72b187520854cff43a
Rlam-git <raul_k-22@hotmail.com>
2023-10-18 12:12:00 -0300

    feat:finalizado o desafio.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -15,6 +13,36 @@ public class Main {
 
     Mentoria mentoria1 = new Mentoria("Mentoria de java","descrição mentoria java", LocalDate.now());
     System.out.println(mentoria1);
+
+    BootCamp bootCamp = new BootCamp();
+    bootCamp.setNome("Bootcamp Java Developer");
+    bootCamp.setDescricao("descrição do bootcamp");
+    bootCamp.getConteudos().add(curso1);
+    bootCamp.getConteudos().add(curso2);
+    bootCamp.getConteudos().add(mentoria1);
+
+    Dev camila = new Dev();
+    camila.setNome("Camila");
+    camila.inscreverBootCamp(bootCamp);
+    camila.progredir();
+    System.out.println("Conteúdos incritos Camila: " + camila.getConteudosInscritos());
+    System.out.println("Conteúdos concluidos Camila: " + camila.getConteudosConcluidos());
+    System.out.println("XP Camila: " + camila.calcularXp());
+
+    System.out.println("--------------------------------------------------------");
+
+    Dev joao = new Dev();
+    joao.setNome("João");
+    joao.inscreverBootCamp(bootCamp);
+    joao.progredir();
+    joao.progredir();
+    System.out.println("Conteúdos incritos João: " + joao.getConteudosInscritos());
+    System.out.println("Conteúdos concluidos João: " + joao.getConteudosConcluidos());
+    System.out.println("XP João: " + joao.calcularXp());
+
+
+
+
 
   }
 
