8b41c91d4ff537b23c504a3626d350e20370adc4
Rlam-git <raul_k-22@hotmail.com>
2023-10-18 11:23:50 -0300

    Feat:Mentoria, Curso e Main class.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Curso curso1 = new Curso("curso java","descrição curso java",8);
+    Curso curso2 = new Curso("curso javascript","descrição curso javascript",8);
+    System.out.println(curso1);
+    System.out.println(curso2);
+
+    Mentoria mentoria1 = new Mentoria("Mentoria de java","descrição mentoria java", LocalDate.now());
+    System.out.println(mentoria1);
+
+  }
+
+}
